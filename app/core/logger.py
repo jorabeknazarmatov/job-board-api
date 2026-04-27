2d89bd339f57120ecee92b8b1ca2cfae19f87330
TURABEK NAZARMATOV <7916947090@my.lerna.uz>
2026-04-28 00:04:53 +0500

    added path for Logs files

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,10 +1,17 @@
 import logging
+from pathlib import Path
+
+
+current_path = Path(__file__).resolve()
+BASE_DIR = current_path.parent.parent.parent
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 
 logger = logging.getLogger("app")
 logger.setLevel(logging.INFO)
 
-file_log = logging.FileHandler("app.log")
+file_log = logging.FileHandler(str(LOG_DIR / "app.log"))
 console_log = logging.StreamHandler()
 
 formatter = logging.Formatter("%(asctime)s || %(levelname)s || %(filename)s:%(lineno)s || %(message)s", datefmt="%d-%m-%Y %H:%M:%S")
