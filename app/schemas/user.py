a6c9466ef6c314fdaf401c5f7ebc2fcd76f8373c
TURABEK NAZARMATOV <7916947090@my.lerna.uz>
2026-04-29 00:18:56 +0500

    Modified user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,20 +1,34 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field, EmailStr
 from datetime import date
+from typing import Annotated
+from enum import Enum
 
+
+BirthDate = Annotated[date, Field(ge=date(1900, 1, 1), le=date.today())]
+
+class SexEnum(str, Enum):
+    MALE = "male"
+    FEMALE = "female"
+    
 
 class UserBase(BaseModel):
-    surname: str
-    name: str
-    middleName: str
-    sex: str
-    birthday: date
-    email: str
+    surname: str = Field(min_length=2, max_length=100)
+    name: str = Field(min_length=2, max_length=100)
+    middleName: str | None = None
+    sex: SexEnum
+    birthday: BirthDate
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=40)
     
     @field_validator("birthday")
     @classmethod
     def check_age(cls, v: date):
-        if v.year < 1900:
-            raise ValueError("Tug'ilgan yil juda kichik")
+        
+        today = date.today()
+        age = today.year - v.year - ((today.month - today.day) - (v.month - v.day))
+        
+        if age < 16:
+            raise ValueError("You must be at least 16 years old to register.")      
         return v
 
 class UserCreate(UserBase):
@@ -22,3 +36,6 @@ class UserCreate(UserBase):
 
 class UserRead(UserBase):
     id: int
+    
+    class Config:
+        from_attributes = True
